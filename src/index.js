f4931c7ff61be4f2fc960725872ec542d0f1f5b5
Amit Kumar <amit@cloud-connect.in>
2019-06-06 18:14:14 +0530

    message

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './login/login';
+
+var destination = document.querySelector(".container");
+
+ReactDOM.render(
+    <Login />,
+    destination
+);
